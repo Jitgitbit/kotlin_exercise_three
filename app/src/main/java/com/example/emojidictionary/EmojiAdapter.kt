cc2ecb953f090d4c2693cfec35e21199c944d586
Jitgitbit <rowowolf@gmail.com>
2020-09-01 17:30:25 +0200

    onBindViewHolder done

--- app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
 class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder> {
 
@@ -15,11 +16,20 @@ class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder> {
         return 10
     }
 
-    override fun onBindViewHolder(holder: TextHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: EmojiAdapter.TextHolder, position: Int) {
+        holder.bindEmoji("😅")
     }
 
     class TextHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+
+        var view : View = v
+        var emoji : String = ""
+
+        fun bindEmoji(emoji:String) {
+            this.emoji = emoji
+            view.itemTextView.text = emoji
+        }
+
         override fun onClick(p0: View?) {
             TODO("Not yet implemented")
         }
