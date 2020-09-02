ca54e2d12d549955abb5ab7ccdde256590ae970d
Jitgitbit <rowowolf@gmail.com>
2020-09-02 13:20:51 +0200

    making the Intent happen

--- app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
@@ -38,6 +39,7 @@ class EmojiAdapter(val emojis: ArrayList<String>) : RecyclerView.Adapter<EmojiAd
 
         override fun onClick(p0: View?) {
             val detailIntent = Intent(view.context, EmojiDetailActivity::class.java)
+            startActivity(view.context,detailIntent,null)
         }
     }
 }
